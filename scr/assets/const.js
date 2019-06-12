29309812335289943de37e1cdde80b8943654396
duyphongqn01 <41832167+duyphongqn01@users.noreply.github.com>
2019-06-13 00:06:23 +0700

    Add files via upload

---
+++ scr/assets/const.js
@@ -0,0 +1,18 @@
+const api = {
+    "WOLFRAM" : 'http://api.wolframalpha.com/v2/result?appid=T8J8YV-H265UQ762K&i=',
+    "MATH": "https://api.trolyfacebook.com/giaitoan/?noidung=",
+    "THOITIET": "https://api.trolyfacebook.com/thoitiet/?noidung=",
+    "COMPILERID1": "http://5f3a947e.compilers.sphere-engine.com/api/v3/submissions/",
+    "COMPILERID2": "/output?access_token=e2f33cc239c13c3fe4d93cea78bec096",
+    "YOUTUBE": "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=AIzaSyBqJsfz5MTmIGmTxjpWeE9YbAFKhBi4JlY&q=",
+    "TRANSLATE": "https://translation.googleapis.com/language/translate/v2?&target=vi&source=en&key=AIzaSyBqJsfz5MTmIGmTxjpWeE9YbAFKhBi4JlY&q=",
+    "GOOGLESEARCH": "https://www.googleapis.com/customsearch/v1?key=AIzaSyCHfJCpWEYUCydDMbb9PqK42XpPQd9L-F8&cx=017207449713114446929:cyipvxn5_rk&q=",
+    "OSUAPI" : "https://osu.ppy.sh/api/get_user?k=f542df9a0b7efc666ac0350446f954740a88faa8&u=",
+    "SIMSIMIAPI": "http://api.simsimi.tech/?text=",
+    "COINMARKET": "https://api.coinmarketcap.com/v1/ticker/",
+	"GOOGLEAUDIO": "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=text.length&client=tw-ob&tl=vi&q=",
+	"GETLINK": "http://nick237.xyz/download-fshare.php?url=",
+	"ANLINK": "http://nick237.xyz/anlink.php?url="
+	
+};
+module.exports = { api };
