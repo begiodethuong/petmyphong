00fe6a7f4a4bdc0b5b1ea5bbf29f397289bd2e33
duyphongqn01 <41832167+duyphongqn01@users.noreply.github.com>
2019-06-17 12:29:29 +0700

    Update const.js

--- scr/assets/const.js
+++ scr/assets/const.js
@@ -8,7 +8,7 @@ const api = {
     "TRANSLATE": "https://translation.googleapis.com/language/translate/v2?&target=vi&source=en&key=AIzaSyBqJsfz5MTmIGmTxjpWeE9YbAFKhBi4JlY&q=",
     "GOOGLESEARCH": "https://www.googleapis.com/customsearch/v1?key=AIzaSyCHfJCpWEYUCydDMbb9PqK42XpPQd9L-F8&cx=017207449713114446929:cyipvxn5_rk&q=",
     "OSUAPI" : "https://osu.ppy.sh/api/get_user?k=f542df9a0b7efc666ac0350446f954740a88faa8&u=",
-    "SIMSIMIAPI": "http://api.simsimi.tech/?text=",
+    "SIMSIMIAPI": "https://sgorki.jdevcloud.com/simt.php?text=",
     "COINMARKET": "https://api.coinmarketcap.com/v1/ticker/",
 	"GOOGLEAUDIO": "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=text.length&client=tw-ob&tl=vi&q=",
 	"GETLINK": "http://nick237.xyz/download-fshare.php?url=",
